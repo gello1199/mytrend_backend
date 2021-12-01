a9be5173d75551f27fd9bccf0d3cd5e94a8b0f91
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-11-30 22:17:09 -0500

    added create method to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,16 @@ class ItemsController < ApplicationController
         render json: items
     end
 
+    def create
+        item = Item.new(item_params)
+        if item.save
+            render json: item
+        else
+            render json: {error: item.errors.full_messages}
+        end
+    end
+
+    
+
 
 end
