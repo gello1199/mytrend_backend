b739f4132e80443d6aafaef8dc0aef61596b3427
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-09 12:34:47 -0500

    changed index to order by created at

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
     def index
-        items = Item.all
+        items = Item.order(:created_at)
         render json: items
     end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
-    # validates :name, :description, :price, :image, :category, presence: true
+    validates :name, :description, :price, :image, :category, presence: true
 
-    # causes a re render
 
-    # set default to false in migration and re-seed
 
 end
