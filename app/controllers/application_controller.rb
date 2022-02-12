ad63bbe18372a85ad10b0d6e22dc1dddb4c034de
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-12 14:40:27 -0500

    added heroku setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::API
     # def render_not_found_response(exception)
     #   render json: { error: exception.message }, status: :not_found
     # end
+
+    def home
+        render json: {message: "Server is up!"}
+    end
+
 end
