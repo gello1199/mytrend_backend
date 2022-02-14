4950ad58cb4df76fb2684bf8ee3aceeb4bb1d2f4
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-14 13:00:15 -0500

    Revert back to netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gello1199.github.io/mytrend-frontend/'
+    origins 'https://mytrend-app.netlify.app/'
 
     resource '*',
       headers: :any,
